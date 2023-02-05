b2feb5e398e908c135908a85b75dbe1d9ee24a83
Raj Kumar Bharti <rajbharti.uday@gmail.com>
2023-02-05 15:32:11 +0530

    initial commit - server running

---
+++ README.md
@@ -0,0 +1,5 @@
+## TechStack
+
+- Docker
+- Express
+- Mongo
---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Docker Express Mongo Demo");
+});
+
+app.listen(port, () => console.log(`Server listening at port ${port}`));
