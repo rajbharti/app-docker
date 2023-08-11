808d47462d3d49079939cb58cebe11fa20d9392c
Raj Kumar Bharti <rajkb.dev@gmail.com>
2023-08-11 10:16:02 +0530

    added single book query route

--- api/src/routes/books.ts
+++ api/src/routes/books.ts
@@ -7,67 +7,92 @@ const collection = "tech";
 
 router
   .route("/")
-  .get(async (req, res) => {
-    try {
-      const db = await getDb();
+  .get((req, res) => {
+    (async () => {
+      try {
+        const db = await getDb();
 
-      const result = await db?.collection(collection).find({}).toArray();
-      res.json(result);
-    } catch (e) {
-      console.error(e);
-      res.status(400).json({ error: "Error in querying books" });
-    } finally {
-      await closeDb();
-    }
+        const result = await db?.collection(collection).find({}).toArray();
+        res.json(result);
+      } catch (e) {
+        console.error(e);
+        res.status(400).json({ error: "Error in querying books" });
+      } finally {
+        await closeDb();
+      }
+    })();
   })
-  .post(async (req, res) => {
-    try {
-      const db = await getDb();
+  .post((req, res) => {
+    (async () => {
+      try {
+        const db = await getDb();
 
-      const result = await db?.collection(collection).insertOne(req.body);
-      res.status(201).send(result);
-    } catch (e) {
-      console.error(e);
-      res.status(400).json({ error: "Error in creating a new book" });
-    } finally {
-      await closeDb();
-    }
+        const result = await db?.collection(collection).insertOne(req.body);
+        res.status(201).send(result);
+      } catch (e) {
+        console.error(e);
+        res.status(400).json({ error: "Error in creating a new book" });
+      } finally {
+        await closeDb();
+      }
+    })();
   });
 
 router
   .route("/:id")
-  .put(async (req, res) => {
-    try {
-      const db = await getDb();
+  .get((req, res) => {
+    (async () => {
+      try {
+        const db = await getDb();
 
-      const result = await db?.collection(collection).updateOne(
-        { _id: new ObjectId(req.params.id) },
-        {
-          $set: req.body,
-        }
-      );
-      res.json(result);
-    } catch (e) {
-      console.error(e);
-      res.status(400).json({ error: "Error in updating a book" });
-    } finally {
-      await closeDb();
-    }
+        const result = await db?.collection(collection).findOne({
+          _id: new ObjectId(req.params.id),
+        });
+        res.status(201).send(result);
+      } catch (e) {
+        console.error(e);
+        res.status(400).json({ error: "Error in querying a book" });
+      } finally {
+        await closeDb();
+      }
+    })();
   })
-  .delete(async (req, res) => {
-    try {
-      const db = await getDb();
+  .put((req, res) => {
+    (async () => {
+      try {
+        const db = await getDb();
 
-      const result = await db
-        ?.collection(collection)
-        .deleteOne({ _id: new ObjectId(req.params.id) });
-      res.send(result);
-    } catch (e) {
-      console.error(e);
-      res.status(400).json({ error: "Error in deleting a book" });
-    } finally {
-      await closeDb();
-    }
+        const result = await db?.collection(collection).updateOne(
+          { _id: new ObjectId(req.params.id) },
+          {
+            $set: req.body,
+          }
+        );
+        res.json(result);
+      } catch (e) {
+        console.error(e);
+        res.status(400).json({ error: "Error in updating a book" });
+      } finally {
+        await closeDb();
+      }
+    })();
+  })
+  .delete((req, res) => {
+    (async () => {
+      try {
+        const db = await getDb();
+
+        const result = await db
+          ?.collection(collection)
+          .deleteOne({ _id: new ObjectId(req.params.id) });
+        res.send(result);
+      } catch (e) {
+        console.error(e);
+        res.status(400).json({ error: "Error in deleting a book" });
+      } finally {
+        await closeDb();
+      }
+    })();
   });
 
 export default router;
